26be8bb462bbe37eda65aa710fa3f84e5e354b55
Christopher Terrano <mterrano1@yahoo.com>
2023-02-06 21:31:02 -0600

    create comment class

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
 
 load_dotenv()
 
 app = Flask(__name__)
-database_url = os.getenv("DATABASE_URL")
-engine = create_engine(database_url)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    case_id = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(500), nullable=False)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
