52f13206821b321db1297be20e8f6ca629fa7f3b
Christopher Terrano <mterrano1@yahoo.com>
2023-02-07 14:57:52 -0600

    create routes and add cors

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
+CORS(app)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,9 +18,32 @@ class Comment(db.Model):
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/cases/<int:case_id>/comments", methods=["GET", "POST"])
+def comments(case_id):
+    if request.method == "GET":
+        # Fetch comments for a specific case
+        comments = Comment.query.filter_by(case_id=case_id).all()
+        comments_list = [{
+            "id": comment.id,
+            "case_id": comment.case_id,
+            "comment": comment.comment,
+            "first_name": comment.first_name,
+            "last_name": comment.last_name
+        } for comment in comments]
+        return jsonify(comments_list)
+
+    if request.method == "POST":
+        # Save a new comment to the database
+        comment_data = request.get_json()
+        new_comment = Comment(
+            case_id=comment_data["case_id"],
+            comment=comment_data["comment"],
+            first_name=comment_data["first_name"],
+            last_name=comment_data["last_name"]
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+        return jsonify({"message": "Comment added successfully."})
 
 if __name__ == '__main__':
     app.run(debug=True)
