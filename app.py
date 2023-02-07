f157fb0add4950baa9fe86f79a0e53d55306328f
Christopher Terrano <mterrano1@yahoo.com>
2023-02-06 21:05:44 -0600

    install psycopg2-binary and add variables to app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
 app = Flask(__name__)
+database_url = os.getenv("DATABASE_URL")
+engine = create_engine(database_url)
+
 
 @app.route("/")
 def hello():
