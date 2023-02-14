db6546e157cf8ec93eebee0d26a5913a3eb47a41
Christopher Terrano <mterrano1@yahoo.com>
2023-02-14 11:06:37 -0600

    add comments to code and README

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# case-tracker-flask
+# Case-Tracker Comment API
+
+This is a Flask API that handles comments for the Case-Tracker project. It allows authenticated users to retrieve and add comments for a specific case.
+
+Installation
+Clone the repository to your local machine.
+Create a virtual environment for the project using virtualenv or conda.
+Activate the virtual environment.
+Install the required packages using pip install -r requirements.txt.
+Create a PostgreSQL database and add the database URI to a .env file as DATABASE_URL.
+Add a secret key for JWT authentication to the .env file as JWT_SECRET.
+Run the database migrations using flask db upgrade.
+Start the application using flask run.
+Endpoints
+GET /comments/:case_id
+Returns a list of comments for a specific case.
+
+Request Parameters:
+
+case_id (int) - the ID of the case to retrieve comments for.
+Response:
+
+200 OK - list of comments as JSON.
+401 Unauthorized - if user is not authenticated.
+POST /comments/:case_id
+Adds a new comment for a specific case.
+
+Request Parameters:
+
+case_id (int) - the ID of the case to add the comment to.
+Request Body:
+
+comment (string) - the comment text.
+first_name (string) - the first name of the user who added the comment.
+last_name (string) - the last name of the user who added the comment.
+Response:
+
+201 Created - if comment is added successfully.
+400 Bad Request - if comment text is missing or empty.
+401 Unauthorized - if user is not authenticated.
+Authentication
+This API uses JWT authentication to verify user identities. To make authenticated requests, clients must include a valid JWT token in the Authorization header of the request. If the token is missing or invalid, the server will respond with a 401 Unauthorized error.
+
+Technologies Used
+Flask - a micro web framework for Python.
+SQLAlchemy - an Object-Relational Mapping (ORM) library for Python.
+Flask-Migrate - a Flask extension that handles database migrations.
+Flask-CORS - a Flask extension for handling Cross-Origin Resource Sharing (CORS).
+PyJWT - a Python library for JSON Web Tokens (JWTs).
+dotenv - a Python library for loading environment variables from a .env file.
--- app.py
+++ app.py
@@ -9,10 +9,14 @@ from flask_migrate import Migrate
 load_dotenv()
 
 app = Flask(__name__)
+# Configures the database URI for SQLAlchemy from environment variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+# Initializes SQLAlchemy and Flask-Migrate extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+# Enables Cross-Origin Resource Sharing (CORS) for the Flask app
 CORS(app)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,28 +25,41 @@ class Comment(db.Model):
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
 
+
+# Authenticates a user using a JSON Web Token (JWT)
 def authenticate_user(request):
+    # Extracts the JWT from the Authorization header
     token = request.headers.get("Authorization", None)
     if not token:
         return None
     try:
+        # Decodes the JWT using the JWT_SECRET environment variable
         decoded_token = jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=["HS256"])
+        # Extracts the user_id from the decoded token
         user_id = decoded_token.get("user_id", None)
         if not user_id:
+            # Returns None if the user_id is missing from the token
             return None
+        # Returns the user_id if authentication succeeds
         return user_id
     except jwt.exceptions.DecodeError:
+        # Returns None if the JWT is invalid
         return None
 
+
+# Defines the GET and POST routes for the comments API
 @app.route("/comments/<int:case_id>", methods=["GET", "POST"])
+# Authenticates the user before proceeding
 def comments(case_id=None):
     user_id = authenticate_user(request)
+    # Returns an error message if authentication fails
     if not user_id:
         return jsonify({"errors": ["Not authorized"]}), 401
 
     if request.method == "GET":
-        # Fetch comments for a specific case
+        # Fetches all comments associated with the given case_id
         comments = Comment.query.filter_by(case_id=case_id).all()
+        # Converts the comments to a list of dictionaries for JSON serialization
         comments_list = [{
             "id": comment.id,
             "case_id": comment.case_id,
@@ -50,6 +67,7 @@ def comments(case_id=None):
             "first_name": comment.first_name,
             "last_name": comment.last_name
         } for comment in comments]
+        # Returns the comments list as a JSON response
         return jsonify(comments_list)
 
     if request.method == "POST":
@@ -57,6 +75,7 @@ def comments(case_id=None):
         comment_data = request.get_json()
         comment = comment_data.get("comment")
         if not comment or comment.strip() == "":
+            # Returns an error message if the comment is missing or empty
             return jsonify({"errors": ["Comment is required"]}), 400
         new_comment = Comment(
             case_id=comment_data["case_id"],
@@ -64,8 +83,10 @@ def comments(case_id=None):
             first_name=comment_data["first_name"],
             last_name=comment_data["last_name"]
         )
+        # Adds the new comment to the database and commits the transaction
         db.session.add(new_comment)
         db.session.commit()
+        # Returns a success message as a JSON response
         return jsonify({"message": "Comment added successfully"}), 201
 
 if __name__ == '__main__':
